12ca6f62155a8a85c25006fdb94f0c04d180f6de
César P <cesarlpb@gmail.com>
2022-12-24 20:35:41 +0000

    Homepage view

--- pc-app/src/App.jsx
+++ pc-app/src/App.jsx
@@ -1,33 +1,45 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import laptop from './assets/img/laptop.png'
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  const [isLogged, setIsLogged] = useState(false)
+  
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <>
+    <div className="main-container container text-center px-0">
+    <nav className="navbar navbar-expand">
+      <a className="navbar-brand" href="#">PC-app</a>
+      <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExample02" aria-controls="navbarsExample02" aria-expanded="false" aria-label="Toggle navigation">
+        <span className="navbar-toggler-icon"></span>
+      </button>
+
+      <div className="collapse navbar-collapse" id="navbarsExample02">
+        <ul className="navbar-nav mr-auto">
+          <li className="nav-item">
+            <a className="nav-link text-white fw-500" href="#">Inicio <span className="sr-only">(current)</span></a>
+          </li>
+          <li className="nav-item">
+            <a className={!isLogged ? "nav-link disabled" : "nav-link"} href="#">Ver stock</a>
+          </li>
+          <li className="nav-item">
+            <a className={!isLogged ? "nav-link disabled" : "nav-link"} href="#">Añadir stock</a>
+          </li>
+        </ul>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    </nav>
+      <h1 className='h1 text-light mt-5'>PC App</h1>
+      {!isLogged && 
+      <div className='my-container'>
+        <div className='col-8'>
+          <img className='col-6 mx-auto' src={laptop} alt="laptop" width={150} />
+          <div className='col-6 mx-auto'>
+          <a href="#" rel='noopener' className='d-inline-block mx-1 btn btn-primary'>Registrarse</a>
+          <a href="#" rel='noopener' className='d-inline-block mx-1 btn btn-primary'>Iniciar sesión</a>
+          </div>
+        </div>        
+      </div>}
     </div>
+    </>
   )
 }
 
